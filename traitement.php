41aaf7c717c6de802f6268c0fbd86b4c00a77be1
johlbaum <julien.ohlbaum@gmail.com>
2024-02-12 01:04:16 +0100

    Insert work art into database

--- traitement.php
+++ traitement.php
@@ -14,4 +14,18 @@ if (
   $description = htmlspecialchars($_POST['description']);
   $artiste = htmlspecialchars($_POST['artiste']);
   $image = htmlspecialchars($_POST['image']);
+
+
+  require "bdd.php";
+  $db = connectToDatabase();
+  $sqlQuery = 'INSERT INTO oeuvres(titre, artiste, image, description) VALUES (:titre, :artiste, :image, :description)';
+  $insertWorkArt = $db->prepare($sqlQuery);
+  $insertWorkArt->execute([
+    'titre' => $titre,
+    'artiste' => $artiste,
+    'image' => $image,
+    'description' => $description,
+  ]);
+
+  header('Location: oeuvre.php?id=' . $db->lastInsertId());
 }
