2bf85cbca122800470cfc2114bdd32689b733fe1
johlbaum <julien.ohlbaum@gmail.com>
2024-03-16 10:43:59 +0100

    Fixing indentation + switch include to require

--- index.php
+++ index.php
@@ -1,21 +1,21 @@
 <?php
-require 'header.php';
-require 'bdd.php';
+  require 'header.php';
+  require 'bdd.php';
 
-$db = connectToDatabase();
-$artWorks = $db->query('SELECT * FROM oeuvres');
+  $db = connexion();
+  $artWorks = $db->query('SELECT * FROM oeuvres ORDER BY id ASC');
 ?>
 
 <div id="liste-oeuvres">
-    <?php foreach ($artWorks as $artWork) : ?>
-        <article class="oeuvre">
-            <a href="oeuvre.php?id=<?= $artWork['id'] ?>">
-                <img src="<?= $artWork['image'] ?>" alt="<?= $artWork['titre'] ?>">
-                <h2><?= $artWork['titre'] ?></h2>
-                <p class="description"><?= $artWork['artiste'] ?></p>
-            </a>
-        </article>
-    <?php endforeach; ?>
+  <?php foreach ($artWorks as $artWork) : ?>
+  <article class="oeuvre">
+    <a href="oeuvre.php?id=<?= $artWork['id'] ?>">
+      <img src="<?= $artWork['image'] ?>" alt="<?= $artWork['titre'] ?>">
+      <h2><?= $artWork['titre'] ?></h2>
+      <p class="description"><?= $artWork['artiste'] ?></p>
+    </a>
+  </article>
+  <?php endforeach; ?>
 </div>
 
 <?php require 'footer.php'; ?>
