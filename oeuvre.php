34db31e0fa7c04148012b48d434187e2cda82ba9
johlbaum <julien.ohlbaum@gmail.com>
2024-03-16 10:40:54 +0100

    Modify redirection to homepage if no results found + switch include to require

--- oeuvre.php
+++ oeuvre.php
@@ -1,33 +1,34 @@
 <?php
-require 'header.php';
-require 'bdd.php';
-$db = connectToDatabase();
+  require 'header.php';
+  require 'bdd.php';
 
-if (empty($_GET['id'])) {
+  $db = connexion();
+
+  if (empty($_GET['id'])) {
     header('Location: index.php');
-}
+  }
 
-$sqlQuery = 'SELECT * FROM oeuvres WHERE id = ?';
-$artWorkStatement = $db->prepare($sqlQuery);
-$artWorkStatement->execute([$_GET['id']]);
-$artWork = $artWorkStatement->fetch();
+  $sqlQuery = 'SELECT * FROM oeuvres WHERE id = ?';
+  $artWorkStatement = $db->prepare($sqlQuery);
+  $artWorkStatement->execute([$_GET['id']]);
+  $artWork = $artWorkStatement->fetch();
 
-if (is_null($artWork)) {
+  if (!$artWork) {
     header('Location: index.php');
-}
+  }
 ?>
 
 <article id="detail-oeuvre">
-    <div id="img-oeuvre">
-        <img src="<?= $artWork['image'] ?>" alt="<?= $artWork['titre'] ?>">
-    </div>
-    <div id="contenu-oeuvre">
-        <h1><?= $artWork['titre'] ?></h1>
-        <p class="description"><?= $artWork['artiste'] ?></p>
-        <p class="description-complete">
-            <?= $artWork['description'] ?>
-        </p>
-    </div>
+  <div id="img-oeuvre">
+    <img src="<?= $artWork['image'] ?>" alt="<?= $artWork['titre'] ?>">
+  </div>
+  <div id="contenu-oeuvre">
+    <h1><?= $artWork['titre'] ?></h1>
+    <p class="description"><?= $artWork['artiste'] ?></p>
+    <p class="description-complete">
+      <?= $artWork['description'] ?>
+    </p>
+  </div>
 </article>
 
 <?php require 'footer.php'; ?>
