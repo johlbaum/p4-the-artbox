c92b8bb3e620d7f1b2f323810710c81a0e121483
johlbaum <julien.ohlbaum@gmail.com>
2024-03-16 10:46:12 +0100

    Directly return the PDO object in the connection function

--- bdd.php
+++ bdd.php
@@ -1,10 +1,8 @@
 <?php
-
-function connectToDatabase() {
-    try {
-        $bdd = new PDO('mysql:host=localhost;dbname=artbox;charset=utf8;', 'root', 'root');
-        return $bdd;
-    } catch (Exception $e) {
-        die('Erreur : ' . $e->getMessage());
-    }
-}
+	function connexion() {
+		try {
+			return new PDO('mysql:host=localhost;dbname=artbox;charset=utf8;', 'root', 'root');
+		} catch (Exception $e) {
+			die('Erreur : ' . $e->getMessage());
+		}
+	}
